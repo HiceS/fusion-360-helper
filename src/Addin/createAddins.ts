1a6e9b98aea3a1d4e4e396473d7e0fdf00311dae
Hices <shawnhice@gmail.com>
2021-02-22 15:20:34 -0800

    Some JSON files for defaults

--- src/Addin/createAddins.ts
+++ src/Addin/createAddins.ts
@@ -3,6 +3,15 @@ import fs = require('fs');
 import path = require('path');
 
 import * as vscode from 'vscode';
+import * as manifest from './defaults/mainfest.json';
+
+/**
+ * 1. Find the Fusion 360 Python Addins Folder
+ * 2. Create a new folder in that location
+ * 3. Generate the Manifest
+ * 4. Generate the Template Code
+ * 5. (Optionally) Allow people to specify template code links
+ */
 
 // This is called by the register command in extension.ts
 export function generatePythonAddin(){
@@ -19,3 +28,14 @@ PLZ SAVE ME
 `;
 
 const defaultFile = `TEST DEFAULT FILE`;
+
+function createFile(filePath: string, fileName: string, contents: string): void{
+    fs.writeFile(path.join(filePath, fileName), contents, err => {
+        if (err){
+            console.error(err);
+            vscode.window.showErrorMessage(`Failed to create file ${fileName}`);
+        }
+
+        vscode.window.showInformationMessage(`Succesfully created file ${fileName}`);
+    });
+}
---
+++ src/Intellisense/envFiles.ts
@@ -0,0 +1,5 @@
+// This is the functions to generate and modify the settings panel
+
+import * as settings from './defaults/settings.json';
+import * as launch from './defaults/launch.json';
+
